5150fcdd83b68d7585ca256ce79647682fbc6898
Seth <swvn10@gmail.com>
2017-06-28 12:54:21 -0400

    Fix something stupid

--- src/main/java/net/swvn9/BotConfig.java
+++ src/main/java/net/swvn9/BotConfig.java
@@ -8,24 +8,24 @@ import java.util.*;
 
 @SuppressWarnings("unused")
 class Config {
-    private static final File Ldir = new File("Logs");
-    private static final File Cdir = new File("Commands");
-    private static final File Config = new File("Config.yml");
-    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-    private static final List<String> Whitetemp = new ArrayList<>();
-    private static String Whitelist[];
-    private static final List<String> AdminTemp = new ArrayList<>();
-    private static String AdminRoles[];
-    static Yaml config;
-    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    private static Map<String,String[]> Groups;
+	private static final File Ldir = new File("Logs");
+	private static final File Cdir = new File("Commands");
+	private static final File Config = new File("Config.yml");
+	private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+	private static final List<String> Whitetemp = new ArrayList<>();
+	private static String Whitelist[];
+	private static final List<String> AdminTemp = new ArrayList<>();
+	private static String AdminRoles[];
+	static Yaml config;
+	@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+	private static Map<String,String[]> Groups;
 
-    static void loadConfig(){
-        try{
-            if(!Cdir.exists()) //noinspection ResultOfMethodCallIgnored
-                Cdir.mkdir();
-            if(!Ldir.exists()) //noinspection ResultOfMethodCallIgnored
-                Ldir.mkdir();
+	static void loadConfig(){
+		try{
+			if(!Cdir.exists()) //noinspection ResultOfMethodCallIgnored
+				Cdir.mkdir();
+			if(!Ldir.exists()) //noinspection ResultOfMethodCallIgnored
+				Ldir.mkdir();
 			/*if(!Config.exists()){
 				FileWriter newFile = new FileWriter(Config);
 				newFile.write("#This file contains all of the configuration options available (for now)"+System.lineSeparator()+"token: Bots Token Here"+System.lineSeparator()+System.lineSeparator()+"adminrole: The admin role ID(s) separated by spaces"+System.lineSeparator()+"whitelist: Channel ID(s) Separated by spaces");
@@ -33,17 +33,17 @@ class Config {
 				System.out.println("The Config.yml file has been created, fill it in with the relevant information.");
 				System.exit(0);
 			}*/
-            net.swvn9.Config.config = mapper.readValue(new File("Config.yml"), Yaml.class);
-        } catch(IOException | NullPointerException ee){
-            System.out.println("There was an error with the configuration file."+System.lineSeparator()+"Please ensure that you copy the \"example_Config.yml\""+System.lineSeparator()+"fill it with your configuration choices"+System.lineSeparator()+"and rename it to \"Config.yml\"");
-            System.out.println("Below is the error message.\u001B[34m"+System.lineSeparator()+ee.getLocalizedMessage()+"\u001B[0m");
-            System.exit(0);
-        }
-        Scanner a = new Scanner(config.getWhitelist());
-        while(a.hasNext()){
-            Whitetemp.add(a.next());
-        }
-        net.swvn9.Config.Whitelist = Whitetemp.toArray(new String[0]);
+			net.swvn9.Config.config = mapper.readValue(new File("Config.yml"), Yaml.class);
+		} catch(IOException | NullPointerException ee){
+			System.out.println("There was an error with the configuration file."+System.lineSeparator()+"Please ensure that you copy the \"example_Config.yml\""+System.lineSeparator()+"fill it with your configuration choices"+System.lineSeparator()+"and rename it to \"Config.yml\"");
+			System.out.println("Below is the error message.\u001B[34m"+System.lineSeparator()+ee.getLocalizedMessage()+"\u001B[0m");
+			System.exit(0);
+		}
+		Scanner a = new Scanner(config.getWhitelist());
+		while(a.hasNext()){
+			Whitetemp.add(a.next());
+		}
+		net.swvn9.Config.Whitelist = Whitetemp.toArray(new String[0]);
 		/*
 		Scanner b = new Scanner(config.getAdminrole());
 		while(b.hasNext()){
@@ -51,108 +51,108 @@ class Config {
 		}
 		net.swvn9.Config.AdminRoles = AdminTemp.toArray(new String[0]);
 		*/
-    }
+	}
 
-    static String getToken(){
-        return config.getToken();
-    }
-    static String getrebrandlyToken(){
-        return config.getRebrandlyToken();
-    }
-    static String getrebrandlyURL(){
-        return config.getRebrandlyURL();
-    }
-    static String[] getWhitelist(){
-        return Whitelist;
-    }
+	static String getToken(){
+		return config.getToken();
+	}
+	static String getrebrandlyToken(){
+		return config.getRebrandlyToken();
+	}
+	static String getrebrandlyURL(){
+		return config.getRebrandlyURL();
+	}
+	static String[] getWhitelist(){
+		return Whitelist;
+	}
 
-    static String[] getAdminRoles(){
-        return AdminRoles;
-    }
-    static String[] getPerms(String Key){
-        return Groups.get(Key);
-    }
-    static String[] getGroups(){
-        String[] keys = new String[Groups.keySet().toArray().length];
-        for(int i = 0; i<Groups.keySet().toArray().length;i++){
-            keys[i] = Groups.keySet().toArray()[i].toString();
-        }
-        return keys;
-    }
+	static String[] getAdminRoles(){
+		return AdminRoles;
+	}
+	static String[] getPerms(String Key){
+		return Groups.get(Key);
+	}
+	static String[] getGroups(){
+		String[] keys = new String[Groups.keySet().toArray().length];
+		for(int i = 0; i<Groups.keySet().toArray().length;i++){
+			keys[i] = Groups.keySet().toArray()[i].toString();
+		}
+		return keys;
+	}
 }
 @SuppressWarnings("unused")
 class configUser {
-    public String id;
-    public boolean admin;
-    public int power;
-    public List<String> permissions;
+	public String id;
+	public boolean admin;
+	public int power;
+	public List<String> permissions;
 }
 @SuppressWarnings("unused")
 class configGroup {
-    public List<String> id;
-    public boolean admin;
-    public int power;
-    public List<String> permissions;
+	public List<String> id;
+	public boolean admin;
+	public int power;
+	public List<String> permissions;
 }
 @SuppressWarnings("unused")
 class Yaml { //this is my yaml bean thingamahooza
 
-    private String Token;
-    //private String Adminrole;
-    private String rebrandlyToken;
-    private String rebrandlyURL;
-    private String Whitelist;
-    private Map<String,configUser> Users;
-    private Map<String,configGroup> Groups;
+	private String Token;
+	//private String Adminrole;
+	private String rebrandlyToken;
+	private String rebrandlyURL;
+	private String Whitelist;
+	private Map<String,configUser> Users;
+	private Map<String,configGroup> Groups;
 
-    String getToken() {
-        return Token;
-    }
-    void setToken(String Token) {
-        this.Token = Token;
-    }
-    /*
-        String getAdminrole() {
-            return Adminrole;
-        }
-        void setAdminrole(String AdminRole) {
-            this.Adminrole = AdminRole;
-        }
-    */
-    String getWhitelist() {
-        return Whitelist;
-    }
-    void setWhitelist(String Whitelist) {
-        this.Whitelist = Whitelist;
-    }
+	String getToken() {
+		return Token;
+	}
+	void setToken(String Token) {
+		this.Token = Token;
+	}
+/*
+	String getAdminrole() {
+		return Adminrole;
+	}
+	void setAdminrole(String AdminRole) {
+		this.Adminrole = AdminRole;
+	}
+*/
+	String getWhitelist() {
+		return Whitelist;
+	}
+	void setWhitelist(String Whitelist) {
+		this.Whitelist = Whitelist;
+	}
 
-    Map<String,configGroup> getGroups(){
-        return Groups;
-    }
-    void setGroups(Map<String,configGroup> Groups){
-        this.Groups = Groups;
-    }
+	Map<String,configGroup> getGroups(){
+		return Groups;
+	}
+	void setGroups(Map<String,configGroup> Groups){
+		this.Groups = Groups;
+	}
 
-    Map<String,configUser> getUsers(){
-        return Users;
-    }
-    void setUsers(Map<String,configUser> Users){
-        this.Users = Users;
-    }
+	Map<String,configUser> getUsers(){
+		return Users;
+	}
+	void setUsers(Map<String,configUser> Users){
+		this.Users = Users;
+	}
 
-    public String getRebrandlyToken() {
-        return rebrandlyToken;
-    }
+	public String getRebrandlyToken() {
+		return rebrandlyToken;
+	}
 
-    public void setRebrandlyToken(String rebrandlyToken) {
-        this.rebrandlyToken = rebrandlyToken;
-    }
+	public void setRebrandlyToken(String rebrandlyToken) {
+		this.rebrandlyToken = rebrandlyToken;
+	}
 
-    public String getRebrandlyURL() {
-        return rebrandlyURL;
-    }
+	public String getRebrandlyURL() {
+		return rebrandlyURL;
+	}
 
-    public void setRebrandlyURL(String rebrandlyURL) {
-        this.rebrandlyURL = rebrandlyURL;
-    }
+	public void setRebrandlyURL(String rebrandlyURL) {
+		this.rebrandlyURL = rebrandlyURL;
+	}
 }
--- src/main/java/net/swvn9/BotListeners.java
+++ src/main/java/net/swvn9/BotListeners.java
@@ -16,109 +16,109 @@ import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 class BotReady implements net.dv8tion.jda.core.hooks.EventListener {
-    @Override
-    public void onEvent(net.dv8tion.jda.core.events.Event event)
-    {
-        if(event instanceof ReadyEvent){
-            for(Guild a : event.getJDA().getGuilds()){
-                switch (a.getId()) {
-                    default:
-                        BotEvent.logger(BotEvent.logPrefix(0) + "I'm in " + a.getName() + "! ID:" + a.getId());
-                        break;
-                    case "123527831664852992":
-                        event.getJDA().getPresence().setStatus(OnlineStatus.INVISIBLE);
-                        BotEvent.logger(BotEvent.logPrefix(0) + "I'm in seb's server!");
-                        break;
-                    case "243112682142695446":
-                        event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
-                        BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the test server!");
-                        break;
-                    case "254861442799370240":
-                        event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
-                        BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the Zamorak Cult public server");
-                        break;
-                    case "319606739550863360":
-                        event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
-                        BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the Zamorak Cult Administer Server");
-                        break;
-                }
-            }
-            BotCommands.bot.start = System.nanoTime();
-        }
-    }
+	@Override
+	public void onEvent(net.dv8tion.jda.core.events.Event event)
+	{
+		if(event instanceof ReadyEvent){
+			for(Guild a : event.getJDA().getGuilds()){
+				switch (a.getId()) {
+					default:
+						BotEvent.logger(BotEvent.logPrefix(0) + "I'm in " + a.getName() + "! ID:" + a.getId());
+						break;
+					case "123527831664852992":
+						event.getJDA().getPresence().setStatus(OnlineStatus.INVISIBLE);
+						BotEvent.logger(BotEvent.logPrefix(0) + "I'm in seb's server!");
+						break;
+					case "243112682142695446":
+						event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
+						BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the test server!");
+						break;
+					case "254861442799370240":
+						event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
+						BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the Zamorak Cult public server");
+						break;
+					case "319606739550863360":
+						event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
+						BotEvent.logger(BotEvent.logPrefix(0) + "I'm in the Zamorak Cult Administer Server");
+						break;
+				}
+			}
+			BotCommands.bot.start = System.nanoTime();
+		}
+	}
 }
 
 @SuppressWarnings("unused")
 class BotEvent extends ListenerAdapter {
 
-    private static final String LITERAL = "::";
-    static String WHITELIST[] = Config.getWhitelist();
-    private static final String LOGTIME = new SimpleDateFormat("MMMDDYY_HHmmss").format(new Date());
+	private static final String LITERAL = "::";
+	static String WHITELIST[] = Config.getWhitelist();
+	private static final String LOGTIME = new SimpleDateFormat("MMMDDYY_HHmmss").format(new Date());
     private static FileWriter log;
     private static Guild Home;
 
-    //check if a specific channel ID is on the whitelist
+	//check if a specific channel ID is on the whitelist
     private boolean channelWhitelisted (String channelID){
         for(String value : WHITELIST){ //for each value in the whitelist array
             if(value.equalsIgnoreCase(channelID)) return true; //if the id is whitelisted, return true.
         }
-        return false; //if it isn't in the whitelist, return false
+    return false; //if it isn't in the whitelist, return false
     }
 
-    //create a prefix for any log entries
-    static String logPrefix(int type){
-        String timeStamp = new SimpleDateFormat("HH:mm:ss").format(new Date()); //ge tthe current timestamp
-        String logType; //prepare the log type string
-        switch(type) {
-            default:
-                logType = "Info"; //Mark the type as Info
-                break;
-            case 1:
-                logType = "Error"; //mark the type as Error
-                break;
-            case 2:
-                logType = "Chat"; //mark the type as Chat
-                break;
-            case 3:
-                logType = "Moji"; //mark the type as Emoji/Reaction
-                break;
-            case 4:
-                logType = "CACH";
-                break;
-            case 5:
-                logType = "DBUG";
-                break;
-            case 6:
-                logType = "Perm";
-                break;
-        }
-        return "["+timeStamp+"] ["+logType+"] [Log]: "; //create and return the log prefix
-    }
+	//create a prefix for any log entries
+	static String logPrefix(int type){
+		String timeStamp = new SimpleDateFormat("HH:mm:ss").format(new Date()); //ge tthe current timestamp
+		String logType; //prepare the log type string
+		switch(type) {
+			default:
+				logType = "Info"; //Mark the type as Info
+				break;
+			case 1:
+				logType = "Error"; //mark the type as Error
+				break;
+			case 2:
+				logType = "Chat"; //mark the type as Chat
+				break;
+			case 3:
+				logType = "Moji"; //mark the type as Emoji/Reaction
+				break;
+			case 4:
+				logType = "CACH";
+				break;
+			case 5:
+				logType = "DBUG";
+				break;
+			case 6:
+				logType = "Perm";
+				break;
+		}
+		return "["+timeStamp+"] ["+logType+"] [Log]: "; //create and return the log prefix
+	}
 
-    @SuppressWarnings("unused")
-    static void logger(String input){
-        try {
-            BotEvent.log =new FileWriter("Logs"+File.separator+"LOG_"+LOGTIME+".txt",true);
-            log.write(input+System.lineSeparator());
-            log.close();
-            System.out.println(input);
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
+	@SuppressWarnings("unused")
+	static void logger(String input){
+		try {
+			BotEvent.log =new FileWriter("Logs"+File.separator+"LOG_"+LOGTIME+".txt",true);
+			log.write(input+System.lineSeparator());
+			log.close();
+			System.out.println(input);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 
-    private static boolean isCommand(Message m){
-        for(int i = 0; i< LITERAL.length(); i++){
-            if(m.getContent().indexOf(LITERAL)!=0){
-                return false;
-            }
-        }
-        return true;
-    }
+	private static boolean isCommand(Message m){
+		for(int i = 0; i< LITERAL.length(); i++){
+			if(m.getContent().indexOf(LITERAL)!=0){
+				return false;
+			}
+		}
+		return true;
+	}
 
     @Override //any message sent that the bot can see
     public void onMessageReceived(MessageReceivedEvent e) {
-        if((e.getAuthor().isBot()||e.getChannelType().equals(ChannelType.PRIVATE))){
+	    if((e.getAuthor().isBot()||e.getChannelType().equals(ChannelType.PRIVATE))){
             return;
         }
         /*
@@ -130,99 +130,99 @@ class BotEvent extends ListenerAdapter {
          */
 
         if(BotCommands.input.waiting&&!e.getMessage().getContent().contains("::input")){
-            return;
-        }
+        	return;
+		}
 
-        if(Home==null) BotEvent.Home= e.getGuild();
+		if(Home==null) BotEvent.Home= e.getGuild();
 
-        String input = e.getMessage().getRawContent();
+		String input = e.getMessage().getRawContent();
         if(e.isFromType(ChannelType.TEXT)) logger(logPrefix(2)+"("+e.getGuild().getName()+", #"+e.getTextChannel().getName()+") "+e.getAuthor().getName()+": "+input);
-        if(e.isFromType(ChannelType.PRIVATE)) logger(logPrefix(2)+"(Private Message) "+e.getAuthor().getName()+": "+input);
+	    if(e.isFromType(ChannelType.PRIVATE)) logger(logPrefix(2)+"(Private Message) "+e.getAuthor().getName()+": "+input);
 
 
-        for(String s:BotCommands.watch.memory){
-            if(input.toLowerCase().contains(s)&&!input.contains("::watch")){
-                TextChannel send = null;
-                for(TextChannel c:e.getGuild().getTextChannels()) {
-                    if (c.getName().contains("logs")) {
-                        send = c;
-                    }
-                }
-                if(send!=null){
-                    EmbedBuilder log = new EmbedBuilder();
-                    log.addField("A watched keyword has been said",s,false);
-                    log.addField("Name",e.getAuthor().getAsMention(),true);
-                    log.addField("Message",input,true);
-                    log.addField("Channel",e.getTextChannel().getAsMention(),true);
-                    log.setColor(new Color(148,168,249));
-                    send.sendMessage(log.build()).queue();
-                    send.sendMessage("@here").complete().delete().queueAfter(1,TimeUnit.SECONDS);
-                }
-            }
-        }
+	    for(String s:BotCommands.watch.memory){
+	    	if(input.toLowerCase().contains(s)&&!input.contains("::watch")){
+				TextChannel send = null;
+				for(TextChannel c:e.getGuild().getTextChannels()) {
+					if (c.getName().contains("logs")) {
+						send = c;
+					}
+				}
+				if(send!=null){
+					EmbedBuilder log = new EmbedBuilder();
+					log.addField("A watched keyword has been said",s,false);
+					log.addField("Name",e.getAuthor().getAsMention(),true);
+					log.addField("Message",input,true);
+					log.addField("Channel",e.getTextChannel().getAsMention(),true);
+					log.setColor(new Color(148,168,249));
+					send.sendMessage(log.build()).queue();
+					send.sendMessage("@here").complete().delete().queueAfter(1,TimeUnit.SECONDS);
+				}
+			}
+		}
 
 
 
-        if(e.getChannel().getId().equals("320615332840472576")&&!e.getAuthor().isBot()){
+	    if(e.getChannel().getId().equals("320615332840472576")&&!e.getAuthor().isBot()){
             if(input.charAt(0)=='<'){
-                BotCommands.v.run(e.getMessage());
+				BotCommands.v.run(e.getMessage());
             }
         }
 
-        if(e.getGuild().getId().equals("319606739550863360")){
-            Scanner message = new Scanner(input);
-            if(input.contains("vote")||input.contains("poll")||input.contains("Vote")||input.contains("Poll")){
-                e.getChannel().sendMessage("<@&319607280540712961>, "+e.getMember().getEffectiveName()+" has called a vote! Leave your vote in the form of a reaction on this message!\n\n"+input).queue(msg->{
-                    while(message.hasNext()){
-                        String z = message.next();
-                        switch(z){
-                            default:
-                                break;
-                            case "\uD83C\uDDE6":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDE7":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDE8":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDE9":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDEA":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDEB":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDEC":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                            case "\uD83C\uDDED":
-                                e.getChannel().addReactionById(msg.getId(),z).queue();
-                                break;
-                        }
-                    }
-                });
-                if (e.getChannelType().isGuild()) e.getMessage().delete().queue();
-            }
-        }
+		if(e.getGuild().getId().equals("319606739550863360")){
+	    	Scanner message = new Scanner(input);
+	    	if(input.contains("vote")||input.contains("poll")||input.contains("Vote")||input.contains("Poll")){
+				e.getChannel().sendMessage("<@&319607280540712961>, "+e.getMember().getEffectiveName()+" has called a vote! Leave your vote in the form of a reaction on this message!\n\n"+input).queue(msg->{
+					while(message.hasNext()){
+						String z = message.next();
+						switch(z){
+							default:
+								break;
+							case "\uD83C\uDDE6":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDE7":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDE8":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDE9":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDEA":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDEB":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDEC":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+							case "\uD83C\uDDED":
+								e.getChannel().addReactionById(msg.getId(),z).queue();
+								break;
+						}
+					}
+				});
+				if (e.getChannelType().isGuild()) e.getMessage().delete().queue();
+			}
+		}
 
 
-        if((isCommand(e.getMessage()))){
-            input=input.replaceFirst(LITERAL,"");
-            Scanner command = new Scanner(input);
-            if(command.hasNext()){
-                for(BotCommand b:BotCommands.commandList){
-                    if(input.toLowerCase().indexOf(b.node.replace("command.","").replace("#all","").toLowerCase())==0&&!input.contains("verify")){
-                        b.run(e.getMessage());
-                        break;
-                    }
-                }
-            }
-            command.close();
-        }
-    }
+		if((isCommand(e.getMessage()))){
+			input=input.replaceFirst(LITERAL,"");
+			Scanner command = new Scanner(input);
+			if(command.hasNext()){
+				for(BotCommand b:BotCommands.commandList){
+					if(input.toLowerCase().indexOf(b.node.replace("command.","").replace("#all","").toLowerCase())==0&&!input.contains("verify")){
+						b.run(e.getMessage());
+						break;
+					}
+				}
+			}
+			command.close();
+		}
+	}
 
 }
